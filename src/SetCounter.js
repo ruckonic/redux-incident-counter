b240fea7ae289febba33a7a75cd48de04006e0a5
Rene Corrales <ruckonic@icloud.com>
2021-07-11 02:41:14 -0600

    add setcounter

--- src/SetCounter.js
+++ src/SetCounter.js
@@ -1,9 +1,16 @@
+import { useDispatch } from 'react-redux';
+import { set } from './actions';
+
 export const SetCounter = () => {
+  const dispatch = useDispatch();
   return (
     <section className="controls">
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          const el = event.target['set-to'];
+          dispatch(set(+el.value));
+          el.value = '';
         }}
       >
         <label htmlFor="set-to">Set Count</label>
